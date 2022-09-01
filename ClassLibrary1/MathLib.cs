b1dfa0692904a67d4f80708b52dcee48069f54b4
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2022-09-01 14:17:11 -0400

    msg

--- ClassLibrary1/MathLib.cs
+++ ClassLibrary1/MathLib.cs
@@ -1,4 +1,7 @@
-﻿namespace ClassLibrary1 // ~ package
+﻿using Humanizer;
+
+
+namespace ClassLibrary1 // ~ package
 {
     public class MathLib // internal accessible in this project= assembly
     {
@@ -9,10 +12,30 @@
             return squaredX;
         }
 
+        /// <summary>
+        /// Adds two numbers and returns them as a English string
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static String Add(int a, int b)
         {
             int val =  a + b;
-            return val.
+            return val.ToWords();
+        }
+
+        // Datatypes
+        // Java 8 primitves, all else classes (byte, short, int, long, float, char, bool, double,
+        // primitive value is passed, not the address
+        // primitives are created on stack (int x;), Objects on heap (new Zigwig())
+
+        // C# Value-types = struct (include primitives and user define value types) VS Reference Types - class
+
+        public int Types()
+        {
+            var x = "9.0f"; // implicit typing
+
+            return 0;
         }
     }
 }
